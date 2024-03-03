68b0e1a9d43ff44a8493adc63ed6e6af0ce23f58
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-03-03 19:16:17 +0530

    solution added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    private boolean isExtraCheese;
+    private boolean isExtraToppings;
+    private boolean isTakeAway;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+        }else{
+            this.price=400;
+        }
+        this.isExtraCheese=false;
+        this.isExtraToppings=false;
+        this.isTakeAway=false;
+        this.bill="";
     }
 
     public int getPrice(){
@@ -17,18 +28,50 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!this.isExtraCheese){
+            this.price = this.price + 80;
+            this.isExtraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!this.isExtraToppings){
+            if(this.isVeg){
+                this.price=this.price + 70;
+            }else{
+                this.price=this.price+120;
+            }
+            this.isExtraToppings=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!this.isTakeAway){
+            this.price=this.price+20;
+            this.isTakeAway=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(isVeg){
+            this.bill+="Base Price of The Pizza: 300\n";
+        }else{
+            this.bill+="Base Price Of The Pizza: 400\n";
+        }
+        if(this.isExtraCheese){
+            this.bill+="Extra Cheese Added: "+80+"\n";
+        }
+        if(this.isExtraToppings){
+            this.bill+="Extra Toppings Added: "+70+"\n";
+        }
+        if(this.isTakeAway){
+            this.bill+="Paperbag Added: "+20+"\n";
+
+        }
+        this.bill+="Total Price: "+this.getPrice()+"\n";
         return this.bill;
     }
 }
